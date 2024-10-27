f440dbdbc6dcb01b446dce266bbfa4cc34c3a879
Chris Mawete <72373055+ChMkd@users.noreply.github.com>
2024-10-27 21:06:16 +0100

    feat(dinogame): adding dinogame

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+
+import React from 'react';
+import Dino from 'react-chrome-dino';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Joue au jeu Chrome Dino !</h1>
+      <Dino />
+    </div>
+  );
+}
+
+export default App;
